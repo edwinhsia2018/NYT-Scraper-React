bcfea58fae4b018ceb3bc25d5fd2924ed2388053
Edwin Hsia <edwinhsia@gmail.com>
2018-05-07 00:42:08 -0500

    sunday updates

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const articleController = require("../../controllers/articleController");
+const articleController = require("../../controllers/articlesController");
 
 // Matches with "/api/articles"
 router.route("/")
