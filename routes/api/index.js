0da04ff66fb736ad2be3aea92af4f023ec8abf45
Edwin Hsia <edwinhsia@gmail.com>
2018-05-08 14:13:26 -0500

    Updating routes 1

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
 const articleRoutes = require("./articles");
+const articlesController = require("../controllers/articlesController")
 
-// Book routes
 router.use("/articles", articleRoutes);
+// Define any API routes first
+// Get saved articles
+router.get("/api/saved", articlesController.find);
+// Save articles
+router.post("/api/saved", articlesController.insert);
+// delete saved articles
+router.delete("/api/saved/:id", articlesController.delete);
+// Send every other request to the React app
 
+router.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 module.exports = router;
